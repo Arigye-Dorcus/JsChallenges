3d8bdefddc23cdd0512e1ae57630f3b13422c31e
Arigye-Dorcus <dorcusarigye8@gmail.com>
2025-04-19 17:13:16 +0300

    add question two and three

---
+++ Set1/ageCheck.js
@@ -0,0 +1,14 @@
+
+let yearOfBirth = prompt("Enter your year of birth:");
+
+let currentYear = new Date().getFullYear();
+
+let age = currentYear - yearOfBirth;
+
+if (age < 18) {
+    console.log("You are a minor.");
+} else if (age >= 18 && age <= 36) {
+    console.log("You are a youth.");
+} else {
+    console.log("You are an elder.");
+}
